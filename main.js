4f3f88ccca023cc65a53828ec1be3b2bd0147d0a
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-10-16 01:28:45 +0530

    BIG VERSION UPDATE!!!
    BIG VERSION UPDATE!!!
    now, Styled Buttons, Added when tutorial not found & more. ALWAYS MERGE!!!

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-//arrays
+// Arrays - Tutorials In a List.
+// You can also use \n or <br> to add a line break.
 tutorialHeaderArray=[
 "Welcome!",
 
 "What are cloud Variables?",
 
-"How to upgrade to scratcher status?","","","","","",""];
+"How to upgrade to scratcher status?"
+,"Tutorial not exist."];
 
 
 tutorialBodyArray=[
@@ -12,14 +14,15 @@ tutorialBodyArray=[
 
 "Cloud Variables are basicly varibles stored on a server. In other words, a cloud variables can be accessed anywhere, but a normal variable can be accessed locally (on your computer) only. On scratch, only a scratcher can obtain cloud variables. Go to the next page to see how to become a scratcher!"
 
-,"When you make a new scratch account, you are a 'New Scratcher'. To have access to cloud variables, you need to become a scratch. It happens usually 2-3 weeks after you make your scratch account. To make it happen faster, share more projects and comment more in Scratch! "
+,"When you make a new scratch account, your status is 'New Scratcher'. To have access to cloud variables, you need to become to be have scratcher status. It happens usually 2-3 weeks after you make your scratch account. To make it happen faster, share more projects and comment more in Scratch! "
+,'The Tutorial you are trying to open does not exist.<div class="large"></div><button id="pBtn" class="navButton" onclick="previous()">Ok, take me back.</button>'];
+// Arrays are Over. See Contributing File For Contributing Info.
 
-,"","","","","","","","","","","","","","","","","","","",""];
-//arrays over
-
-//variables
+// Intialize Variables
 tutorialPageCounter = 1;
 tutorialArrayCounter = 0;
+tutorialNo = 1;
+totalPages = 3;
 
 //variables over
 //functions
@@ -29,12 +32,22 @@ function next(){
 
     document.getElementById("tutorialHead").innerHTML=tutorialHeaderArray[tutorialArrayCounter];
     document.getElementById("tutorialText").innerHTML=tutorialBodyArray[tutorialArrayCounter];
+    tutorialNo++;
+    // alert(tutorialNo);
 }
 
 function previous(){
-    tutorialPageCounter = tutorialPageCounter - 1;
-    tutorialArrayCounter = tutorialArrayCounter - 1;
+    if(!tutorialNo == -1){
+        tutorialPageCounter = tutorialPageCounter - 1;
+        tutorialArrayCounter = tutorialArrayCounter - 1;
 
-    document.getElementById("tutorialHead").innerHTML=tutorialHeaderArray[tutorialArrayCounter];
-    document.getElementById("tutorialText").innerHTML=tutorialBodyArray[tutorialArrayCounter];
+        document.getElementById("tutorialHead").innerHTML=tutorialHeaderArray[tutorialArrayCounter];
+        document.getElementById("tutorialText").innerHTML=tutorialBodyArray[tutorialArrayCounter];
+        tutorialNo--;
+        // alert(tutorialNo);
+    }
+    else{
+        console.log("Can't go previous to Page No. -1.");
+        alert("Can't go previous to Page No. -1.");
+    }
 }
